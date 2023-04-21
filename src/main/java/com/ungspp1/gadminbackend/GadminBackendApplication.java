b354ae61927aa62480a08ceee5d60b0288b75432
Nacho <conte.ignacio@hotmail.com>
2023-04-21 14:06:07 -0300

    Commit inicial

---
+++ src/main/java/com/ungspp1/gadminbackend/GadminBackendApplication.java
@@ -0,0 +1,13 @@
+package com.ungspp1.gadminbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GadminBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GadminBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ungspp1/gadminbackend/GadminBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ungspp1.gadminbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GadminBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
