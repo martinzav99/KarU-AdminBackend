05c665fca5037ccf0b7f4001be790bca76938801
GastonOlivera <gastoncarp2012@gmail.com>
2023-04-30 20:56:44 -0300

    arregalar lo que no funcionaba sobre base de datos

--- src/main/java/com/ungspp1/gadminbackend/config/DatabaseConfig.java
+++ src/main/java/com/ungspp1/gadminbackend/config/DatabaseConfig.java
@@ -1,41 +1,41 @@
-// package com.ungspp1.gadminbackend.config;
+package com.ungspp1.gadminbackend.config;
 
-// import org.slf4j.Logger;
-// import org.slf4j.LoggerFactory;
-// import org.springframework.beans.factory.annotation.Value;
-// //import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-// //import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.context.annotation.Configuration;
-// import javax.sql.DataSource;
-// import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+//import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import javax.sql.DataSource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
-// //import lombok.extern.log4j.Log4j;
+//import lombok.extern.log4j.Log4j;
 
 
-// @Configuration
-// public class DatabaseConfig {
+@Configuration
+public class DatabaseConfig {
  
-//     @Value("${spring.datasource.url}")
-//     private String datasourceUrl;
+    @Value("${spring.datasource.url}")
+    private String datasourceUrl;
  
-//     @Value("${spring.datasource.username}")
-//     private String datasourceUsername;
+    @Value("${spring.datasource.username}")
+    private String datasourceUsername;
  
-//     @Value("${spring.datasource.password}")
-//     private String datasourcePassword;
+    @Value("${spring.datasource.password}")
+    private String datasourcePassword;
     
-//     private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConfig.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(DatabaseConfig.class);
     
-//     @Bean
-//     public DataSource dataSource() {
-//         DriverManagerDataSource dataSource = new DriverManagerDataSource();
-//         dataSource.setDriverClassName("org.postgresql.Driver");
-//         dataSource.setUrl(datasourceUrl);
-//         dataSource.setUsername(datasourceUsername);
-//         dataSource.setPassword(datasourcePassword);
-//         LOGGER.info("conexion exitosa");
-//         return  dataSource;
-//     }
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.postgresql.Driver");
+        dataSource.setUrl(datasourceUrl);
+        dataSource.setUsername(datasourceUsername);
+        dataSource.setPassword(datasourcePassword);
+        LOGGER.info("conexion exitosa");
+        return  dataSource;
+    }
  
-// }
+}
