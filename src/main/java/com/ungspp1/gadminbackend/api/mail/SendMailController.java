cc49884a78682729824c40a00296d1b96f067179
GastonOlivera <gastoncarp2012@gmail.com>
2023-05-02 18:45:29 -0300

    java mail sender

---
+++ src/main/java/com/ungspp1/gadminbackend/api/mail/SendMailController.java
@@ -0,0 +1,31 @@
+package com.ungspp1.gadminbackend.api.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ungspp1.gadminbackend.response.BaseBodyResponse;
+import com.ungspp1.gadminbackend.response.ResponseHelper;
+
+
+@RestController
+@RequestMapping("/api/v1/login")
+public class SendMailController {
+    
+    @Autowired
+    SendMailFacade facade;
+
+    
+    @PostMapping(value = "/sendConfirmationMail", produces = {"application/json"})
+    public ResponseEntity<BaseBodyResponse<?>> getSession(){
+        try{
+            return ResponseHelper.simpleResponse(facade.sendAutentcathionMail());
+        } catch (Exception e) {
+            return ResponseHelper.errorResponse(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        }
+    }
+}
