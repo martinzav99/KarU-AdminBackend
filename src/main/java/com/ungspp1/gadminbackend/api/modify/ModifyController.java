cac2d11fc19bc411561368a01b211d4d20534194
Nacho <conte.ignacio@hotmail.com>
2023-05-09 16:52:44 -0300

    Cross origin en signup y update

--- src/main/java/com/ungspp1/gadminbackend/api/modify/ModifyController.java
+++ src/main/java/com/ungspp1/gadminbackend/api/modify/ModifyController.java
@@ -3,6 +3,7 @@ package com.ungspp1.gadminbackend.api.modify;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import com.ungspp1.gadminbackend.restResponse.ResponseHelper;
 
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/user")
 public class ModifyController 
 {
