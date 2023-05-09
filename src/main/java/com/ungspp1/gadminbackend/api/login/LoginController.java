c7bfc9c60bbe8bc903b29c5c22b4b22250271e76
Alanknm1 <alan.knm1@gmail.com>
2023-05-09 15:55:30 -0300

    login cors error fixed

--- src/main/java/com/ungspp1/gadminbackend/api/login/LoginController.java
+++ src/main/java/com/ungspp1/gadminbackend/api/login/LoginController.java
@@ -3,6 +3,7 @@ package com.ungspp1.gadminbackend.api.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.ungspp1.gadminbackend.restResponse.BaseBodyResponse;
 import com.ungspp1.gadminbackend.restResponse.ResponseHelper;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/api/v1/login")
 public class LoginController {
     
