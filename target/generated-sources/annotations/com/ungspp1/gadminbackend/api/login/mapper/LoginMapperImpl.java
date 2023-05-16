69cc46a68e886141bab6c6b562eb7d7398ab2b71
Martin <martinkapo99@gmail.com>
2023-05-16 09:33:49 -0300

    nuevo dockerfile

--- target/generated-sources/annotations/com/ungspp1/gadminbackend/api/login/mapper/LoginMapperImpl.java
+++ target/generated-sources/annotations/com/ungspp1/gadminbackend/api/login/mapper/LoginMapperImpl.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-16T03:17:34-0300",
+    date = "2023-05-16T09:31:43-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230413-0857, environment: Java 17.0.7 (Eclipse Adoptium)"
 )
 @Component
--- target/generated-sources/annotations/com/ungspp1/gadminbackend/api/signup/mapper/SignupUserMapperImpl.java
+++ target/generated-sources/annotations/com/ungspp1/gadminbackend/api/signup/mapper/SignupUserMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2023-05-16T03:17:37-0300",
+    date = "2023-05-16T09:31:47-0300",
     comments = "version: 1.5.5.Final, compiler: Eclipse JDT (IDE) 3.34.0.v20230413-0857, environment: Java 17.0.7 (Eclipse Adoptium)"
 )
 @Component
