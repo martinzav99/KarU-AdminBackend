e071074e1a92a2e305e4ddd8bf913836052f9d02
Martin <martinkapo99@gmail.com>
2023-05-16 13:02:30 -0300

    prueba railway

--- src/main/java/com/ungspp1/gadminbackend/GadminBackendApplication.java
+++ src/main/java/com/ungspp1/gadminbackend/GadminBackendApplication.java
@@ -2,8 +2,9 @@ package com.ungspp1.gadminbackend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class GadminBackendApplication {
  
 	public static void main(String[] args) {
